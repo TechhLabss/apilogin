f6de0ca0bb55effad09c9ea8ef28dee01c95851c
Amanda <labgerenciamento@gmail.com>
2022-10-19 16:17:23 -0300

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('../database');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    senha: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
